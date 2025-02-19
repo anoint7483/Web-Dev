42473571f8a9b8afa89dc917d394ea046d8334ae
anoint7483 <yabhishek8774@gmail.com>
2025-02-19 23:35:55 +0530

    Revised functions and Dom manipulation

--- Frontend/JavaScript/script.js
+++ Frontend/JavaScript/script.js
@@ -1,1 +1,31 @@
-let div = document.querySelector("div");
+let btnl = document.querySelector("#btnl");
+console.log(btnl);
+
+// btnl.addEventListener("click", (evt) => {
+//     console.log("button was clicked");
+//     console.log(evt);
+//     console.log(evt.type);
+// });
+
+let currMode = "light";
+
+btnl.addEventListener("click", () => {
+    // console.log("button was clicked - 2");
+
+    if(currMode === "light") {
+        currMode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
+    } else {
+        currMode = "light";
+        document.querySelector("body").style.backgroundColor = "white";
+
+    }
+
+    console.log(currMode);
+});
+
+// const handler = () => {
+//     console.log("button was clicked - 10")
+// }
+
+// btnl.addEventListener("click", handler);
